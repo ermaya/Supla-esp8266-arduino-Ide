3a4425fcc2abe6e9b94c881caed820e6521c81c9
Maya <34374601+ermaya@users.noreply.github.com>
2018-07-13 02:38:49 +0200

    Add files via upload

---
+++ supla_3BTN_3Relay_ntc_reset_to_Config/supla_3BTN_3Relay_ntc_reset_to_Config.ino
@@ -0,0 +1,386 @@
+#include <FS.h>  
+
+#include <ESP8266WiFi.h>
+#define SUPLADEVICE_CPP
+#include <SuplaDevice.h>
+
+
+#include <ESP8266WebServer.h>
+#include <DNSServer.h>
+#include <WiFiManager.h> 
+#include <ArduinoJson.h>
+#include <DoubleResetDetector.h> 
+// Number of seconds after reset during which a 
+// subseqent reset will be considered a double reset.
+#define DRD_TIMEOUT 30 //30 second to enter wifi confic whit reset Btn
+
+// RTC Memory Address for the DoubleResetDetector to use
+#define DRD_ADDRESS 0
+
+DoubleResetDetector drd(DRD_TIMEOUT, DRD_ADDRESS);
+
+
+#define BTN_COUNT 3
+
+WiFiClient client;
+//define your default values here, if there are different values in config.json, they are overwritten.
+//length should be max size + 1 
+char Supla_server[40];
+char Location_id[15];
+char Location_Pass[20];
+byte mac[6];
+
+//flag for saving data
+bool shouldSaveConfig = false;
+bool initialConfig = false;
+
+int timeout           = 120; // seconds to run for wifi config
+
+unsigned int Rs = 100000;  //ntc serie resistor
+double Vcc = 3.3;          //ntc supli voltage
+
+//callback notifying us of the need to save config
+void saveConfigCallback () {
+  Serial.println("Should save config");
+  shouldSaveConfig = true;
+}
+void ondemandwifiCallback () {
+// The extra parameters to be configured (can be either global or just in the setup)
+  // After connecting, parameter.getValue() will get you the configured value
+  // id/name placeholder/prompt default length
+  WiFiManagerParameter custom_Supla_server("server", "supla server", Supla_server, 40);
+  WiFiManagerParameter custom_Location_id("ID", "Location_id", Location_id, 15);
+  WiFiManagerParameter custom_Location_Pass("Password", "Location_Pass", Location_Pass, 20);
+
+  //WiFiManager
+  //Local intialization. Once its business is done, there is no need to keep it around
+  WiFiManager wifiManager;
+
+  //set config save notify callback
+  wifiManager.setSaveConfigCallback(saveConfigCallback);
+  
+  //add all your parameters here
+  wifiManager.addParameter(&custom_Supla_server);
+  wifiManager.addParameter(&custom_Location_id);
+  wifiManager.addParameter(&custom_Location_Pass);
+
+  //set minimu quality of signal so it ignores AP's under that quality
+  //defaults to 8%
+  wifiManager.setMinimumSignalQuality();
+
+  // set configportal timeout
+    wifiManager.setConfigPortalTimeout(timeout);
+
+    if (!wifiManager.startConfigPortal("OnDemandAP")) {
+      Serial.println("failed to connect and hit timeout");
+      delay(3000);
+      //reset and try again, or maybe put it to deep sleep
+      ESP.restart();
+      delay(5000);
+    }
+    //if you get here you have connected to the WiFi
+    Serial.println("connected...yeey :)");
+    
+    //read updated parameters
+    strcpy(Supla_server, custom_Supla_server.getValue());
+    strcpy(Location_id, custom_Location_id.getValue());
+    strcpy(Location_Pass, custom_Location_Pass.getValue());
+    
+  WiFi.softAPdisconnect(true);   //  close AP
+}
+
+// DS18B20 Sensor read implementation---------------------------------- Temp -------------------------------------------
+double get_temperature(int channelNumber, double last_val) {  // 10k Ntc Adc to Gnd and 100k Resistor Adc to +3,3v
+    
+      int val = 0;
+      for(int i = 0; i < 20; i++) {
+      val += analogRead(A0);
+      delay(1);
+      }
+      val = val / 20;
+      double V_NTC = (double)val / 1024;
+      double R_NTC = (Rs * V_NTC) / (Vcc - V_NTC);
+      R_NTC = log(R_NTC);
+      double t = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * R_NTC * R_NTC ))* R_NTC );
+      t = t - 273.15;
+
+    
+      return t;  
+}
+typedef struct {  //------------------------------------------- BTN ----------------------------------------------------
+  int pin;
+  int relay_pin;
+  int channel;
+  char last_val;
+  int ms;
+  unsigned long last_time;
+} _btn_t;
+
+_btn_t btn[BTN_COUNT];
+
+void supla_timer() {
+  char v;
+  unsigned long now = millis();
+  
+  for(int a=0;a<BTN_COUNT;a++)
+    if (btn[a].pin > 0) {
+        v = digitalRead(btn[a].pin);
+        if (v != btn[a].last_val && now - btn[a].last_time ) {
+           btn[a].last_val = v;
+           btn[a].last_time = now;
+           if (v==0)
+             {
+              if ( btn[a].ms > 0 ) {
+                     SuplaDevice.relayOn(btn[a].channel, btn[a].ms);
+                     Serial.println(" monostable");
+                 } else {
+                 if ( digitalRead(btn[a].relay_pin) == 0 ) {
+                  SuplaDevice.relayOff(btn[a].channel);
+                  Serial.print("BTN Switsh off relay ");
+                  Serial.println(btn[a].relay_pin);
+                 } else {
+                  SuplaDevice.relayOn(btn[a].channel, 0);
+                  Serial.print("BTN Switsh on relay ");
+                  Serial.println(btn[a].relay_pin);
+                 }        
+             }
+        }
+      }
+    }
+}
+void supla_btn_init() {
+  for(int a=0;a<BTN_COUNT;a++)
+    if (btn[a].pin > 0) {
+        pinMode(btn[a].pin, INPUT_PULLUP);
+        btn[a].last_val = digitalRead(btn[a].pin);
+        btn[a].last_time = millis();
+    }
+}
+
+void setup() {  //------------------------------------------------ Setup ----------------------------------------------
+
+  Serial.begin(115200);
+  //sensors.begin();
+
+   if (drd.detectDoubleReset()) {
+    Serial.println("Double Reset Detected");
+    ondemandwifiCallback ();
+  } else {
+    Serial.println("No Double Reset Detected");
+  }
+
+  
+  if (WiFi.SSID()==""){
+    //Serial.println("We haven't got any access point credentials, so get them now");   
+    initialConfig = true;
+  }
+  
+  //read configuration from FS json
+  Serial.println("mounting FS...");
+  
+  if (SPIFFS.begin()) {
+    Serial.println("mounted file system");
+    if (SPIFFS.exists("/config.json")) {
+      //file exists, reading and loading
+      Serial.println("reading config file");
+      File configFile = SPIFFS.open("/config.json", "r");
+      if (configFile) {
+        Serial.println("opened config file");
+        size_t size = configFile.size();
+        // Allocate a buffer to store contents of the file.
+        std::unique_ptr<char[]> buf(new char[size]);
+
+        configFile.readBytes(buf.get(), size);
+        DynamicJsonBuffer jsonBuffer;         
+        JsonObject& json = jsonBuffer.parseObject(buf.get());
+        //json.printTo(Serial);   //print config data to serial on startup
+        if (json.success()) {
+          Serial.println("\nparsed json");
+
+          strcpy(Supla_server, json["Supla_server"]);
+          strcpy(Location_id, json["Location_id"]);
+          strcpy(Location_Pass, json["Location_Pass"]);
+
+        } else {
+          Serial.println("failed to load json config");
+          
+        }
+      }
+    }
+  } else {
+    Serial.println("failed to mount FS");
+  }
+  WiFi.mode(WIFI_STA); // Force to station mode because if device was switched off while in access point mode it will start up next time in access point mode.
+
+   char GUID[SUPLA_GUID_SIZE] = {0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00,0x00};  // ﻿with GUID that you can retrieve from https://www.supla.org/arduino/get-guid.
+   
+   WiFi.macAddress(mac);  // Get mac
+   
+   
+  //CHANNEL0 - RELAY                 
+  SuplaDevice.addRelay(5, true); //D1
+  //CHANNEL1 - RELAY              
+  SuplaDevice.addRelay(4,true);  //D2
+  //CHANNEL2 - RELAY              
+  SuplaDevice.addRelay(D4,true);  //D4 Gpio2
+  // CHANNEL3 - Thermometer DS18B20
+  SuplaDevice.addDS18B20Thermometer();
+
+  memset(btn, 0, sizeof(btn));
+  btn[0].pin =14;         // pin gpio del boton D5
+  btn[0].relay_pin =5;    // pin gpio del rele corespondiente D1
+  btn[0].channel =0;      // canal de supla
+  btn[0].ms =0;           // 0 = canal biestable o canal monoestable activo por xx ms    if = 0 Bistable -- if > 0 Monostable for X ms
+  btn[1].pin =12;         // pin gpio del boton D6
+  btn[1].relay_pin =4;    // pin gpio del rele corespondiente D2
+  btn[1].channel =1;      // canal de supla
+  btn[1].ms =0;           // 0 = canal biestable o canal monoestable activo por xx ms
+  btn[2].pin =D7;         // pin gpio del boton D7 Gpio13
+  btn[2].relay_pin =D4;   // pin gpio del rele corespondiente D4 Gpio02
+  btn[2].channel =2;      // canal de supla
+  btn[2].ms =120000;      // 0 = canal biestable o canal monoestable activo por xx ms
+  supla_btn_init();
+  SuplaDevice.setTimerFuncImpl(&supla_timer);
+  SuplaDevice.setName("elmaya esp8266");
+
+  int LocationID = atoi(Location_id);
+  SuplaDevice.begin(GUID,              // Global Unique Identifier 
+                    mac,               // Ethernet MAC address
+                    Supla_server,  // SUPLA server address
+                    LocationID,                 // Location ID 
+                    Location_Pass);               // Location Password
+    
+}
+
+void loop() {
+
+  // Call the double reset detector loop method every so often,
+  // so that it can recognise when the timeout expires.
+  // You can also call drd.stop() when you wish to no longer
+  // consider the next reset as a double reset.
+  drd.loop();
+  
+  //save the custom parameters to FS
+  if (shouldSaveConfig) {
+    Serial.println("saving config");
+    DynamicJsonBuffer jsonBuffer;
+    JsonObject& json = jsonBuffer.createObject();
+    json["Supla_server"] = Supla_server;
+    json["Location_id"] = Location_id;
+    json["Location_Pass"] = Location_Pass;
+
+    File configFile = SPIFFS.open("/config.json", "w");
+    if (!configFile) {
+      Serial.println("failed to open config file for writing");
+    }
+    json.prettyPrintTo(Serial);
+    json.printTo(configFile);
+    configFile.close();
+    Serial.println("config saved");
+    shouldSaveConfig = false;
+    //end save
+  }
+  
+  if (WiFi.status() != WL_CONNECTED) 
+  {
+    WiFi_up();
+  }
+  
+  SuplaDevice.iterate();
+  SuplaDevice.setTemperatureCallback(&get_temperature);
+}
+
+
+// Supla.org ethernet layer
+    int supla_arduino_tcp_read(void *buf, int count) {
+        _supla_int_t size = client.available();
+       
+        if ( size > 0 ) {
+            if ( size > count ) size = count;
+            return client.read((uint8_t *)buf, size);
+        };
+    
+        return -1;
+    };
+    
+    int supla_arduino_tcp_write(void *buf, int count) {
+        return client.write((const uint8_t *)buf, count);
+    };
+    
+    bool supla_arduino_svr_connect(const char *server, int port) {
+          return client.connect(server, 2015);
+    }
+    
+    bool supla_arduino_svr_connected(void) {
+          return client.connected();
+    }
+    
+    void supla_arduino_svr_disconnect(void) {
+         client.stop();
+    }
+    
+    void supla_arduino_eth_setup(uint8_t mac[6], IPAddress *ip) {
+
+       WiFi_up();
+    }
+
+SuplaDeviceCallbacks supla_arduino_get_callbacks(void) {
+          SuplaDeviceCallbacks cb;
+          
+          cb.tcp_read = &supla_arduino_tcp_read;
+          cb.tcp_write = &supla_arduino_tcp_write;
+          cb.eth_setup = &supla_arduino_eth_setup;
+          cb.svr_connected = &supla_arduino_svr_connected;
+          cb.svr_connect = &supla_arduino_svr_connect;
+          cb.svr_disconnect = &supla_arduino_svr_disconnect;
+          cb.get_temperature = &get_temperature;
+           cb.get_temperature_and_humidity = NULL;
+          cb.get_rgbw_value = NULL;
+          cb.set_rgbw_value = NULL;
+          
+          return cb;
+}
+
+void WiFi_up() // Procedimiento de conexión para redes WiFi
+{
+  Serial.print("Conexión a la red ");
+ // Serial.println(ssid);
+
+  WiFi.begin(); // Intentar conectarse a la red
+
+
+  for (int x = 60; x > 0; x--) 
+  {
+    if (WiFi.status() == WL_CONNECTED) 
+    {
+      break;                           
+    }
+    else                                 
+    {
+      Serial.print(".");                
+      delay(500);                      
+    }
+
+  }
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    Serial.println("");
+    Serial.println("Conexión hecha");
+    Serial.println("Adres IP: ");
+    Serial.print(WiFi.localIP());
+    Serial.print(" / ");
+    Serial.println(WiFi.subnetMask());
+    Serial.print("puerta: ");
+    Serial.println(WiFi.gatewayIP());
+    long rssi = WiFi.RSSI();
+    Serial.print("Fuerza de la señal (RSSI): ");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+  }
+  else    
+  {
+    Serial.println("");
+    Serial.println("La conexión no pudo hacerse");
+  }
+}
