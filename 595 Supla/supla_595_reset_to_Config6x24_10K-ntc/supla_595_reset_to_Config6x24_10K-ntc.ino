a7f94adbe19aab4f966b4a0f63398c5cf6620119
Maya <34374601+ermaya@users.noreply.github.com>
2019-01-24 12:34:22 +0100

    Update supla_595_reset_to_Config6x24_10K-ntc.ino

--- 595 Supla/supla_595_reset_to_Config6x24_10K-ntc/supla_595_reset_to_Config6x24_10K-ntc.ino
+++ 595 Supla/supla_595_reset_to_Config6x24_10K-ntc/supla_595_reset_to_Config6x24_10K-ntc.ino
@@ -666,6 +666,11 @@ void loop() {
 
   drd.loop();
 
+  if (initialConfig){
+    ondemandwifiCallback () ;
+    initialConfig = false; 
+  }
+
   if (shouldSaveConfig) {
     Serial.println("saving config");
     DynamicJsonBuffer jsonBuffer;
@@ -683,7 +688,9 @@ void loop() {
     configFile.close();
     Serial.println("config saved");
     shouldSaveConfig = false;
-    //end save
+    WiFi.mode(WIFI_STA);
+    ESP.restart();
+    delay(5000); 
   }
   if (WiFi.status() != WL_CONNECTED) { 
     WiFi_up();
