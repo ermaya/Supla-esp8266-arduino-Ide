9e3e8d8d82ace217605d10e264f5f9518543bd5d
Maya <34374601+ermaya@users.noreply.github.com>
2019-11-12 18:50:10 +0100

    Add files via upload

---
+++ RGB/libraries/ESPEFC-master/examples/WebUpdater/WebUpdater.ino
@@ -0,0 +1,54 @@
+/*
+  To upload through terminal you can use: curl -F "image=@firmware.bin" esp8266-webupdate.local/update
+*/
+
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+#include <ESPEFC.h>
+
+const char* host = "esp8266";
+const char* ssid = ".......";
+const char* password = ".......";
+
+ESP8266WebServer httpServer(80);
+ESPEFC httpUpdater;
+
+void setup(void) {
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Booting Sketch...");
+  WiFi.mode(WIFI_AP_STA);
+  WiFi.begin(ssid, password);
+  for (int x = 60; x > 0; x--) 
+  {
+    if (WiFi.status() != WL_CONNECTED)     
+      Serial.print(".");                
+      delay(500);                      
+  }
+    Serial.println("");
+    Serial.println("CONNECTED");
+    Serial.print("local IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.print("subnetMask: ");
+    Serial.println(WiFi.subnetMask());
+    Serial.print("gatewayIP: ");
+    Serial.println(WiFi.gatewayIP());
+    long rssi = WiFi.RSSI();
+    Serial.print("Signal Strength (RSSI): ");
+    Serial.print(rssi);
+    Serial.println(" dBm");
+
+  MDNS.begin(host);
+  httpUpdater.setup(&httpServer);
+  httpServer.begin();
+
+  MDNS.addService("http", "tcp", 80);
+  Serial.printf("HTTPUpdateServer ready! Open http://%s.local/update in your browser\n", host);
+}
+
+void loop(void) {
+  httpServer.handleClient();
+}
