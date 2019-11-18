34f0e6e78c367aace407de5a8a45971d717fab7a
Maya <34374601+ermaya@users.noreply.github.com>
2019-11-18 20:41:39 +0100

    Add files via upload

---
+++ libraries/SonOff Si7021 librarie/SI7021/examples/SI7021_Test/SI7021_Test.ino
@@ -0,0 +1,32 @@
+#include "SI7021.h"
+
+DHT si7021;
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
+
+  si7021.setup(5); // data pin 2
+}
+
+void loop()
+{
+  delay(3000);
+
+  float humidity = si7021.getHumidity();
+  float temperature = si7021.getTemperature();
+
+  Serial.println(si7021.getModel());
+
+  Serial.print(si7021.getStatusString());
+  Serial.print("\t");
+  Serial.print(humidity, 1);
+  Serial.print("\t\t");
+  Serial.print(temperature, 1);
+  Serial.print("\t\t");
+  Serial.println(si7021.toFahrenheit(temperature), 1);
+}
+
+
