961e769ea67067c49e966a3eb570800ba85e686b
Maya <34374601+ermaya@users.noreply.github.com>
2019-12-21 21:37:03 +0100

    Add files via upload

---
+++ Test_guid/Test_guid.ino
@@ -0,0 +1,89 @@
+#include <EEPROM.h>
+#include "ESP8266TrueRandom.h"
+
+char GUID[SUPLA_GUID_SIZE];
+char AUTHKEY[SUPLA_AUTHKEY_SIZE];
+byte uuidNumber[16]; // UUIDs in binary form are 16 bytes long
+
+void guid_authkey(void) {
+  if (EEPROM.read(300) != 60){
+    int eep_gui = 301;
+
+    ESP8266TrueRandom.uuid(uuidNumber);
+
+    String uuidString = "";
+    for (int i = 0; i < 16; i++) {
+      int topDigit = uuidNumber[i] >> 4;
+      int bottomDigit = uuidNumber[i] & 0x0f;
+      uuidString += "0123456789abcdef"[topDigit];
+      uuidString += "0123456789abcdef"[bottomDigit];
+    }
+    int length_uuid = uuidString.length();
+    for (int i = 0; i < length_uuid; ++i) {
+      EEPROM.put(eep_gui + i, uuidString[i]);
+    }
+
+    int eep_aut = 321;
+
+    ESP8266TrueRandom.uuid(uuidNumber);
+
+    String uuidString2 = "";
+    for (int i = 0; i < 16; i++) {
+      int topDigit = uuidNumber[i] >> 4;
+      int bottomDigit = uuidNumber[i] & 0x0f;
+      uuidString2 += "0123456789abcdef"[topDigit];
+      uuidString2 += "0123456789abcdef"[bottomDigit];
+    }
+    int length_uuid2 = uuidString2.length();
+    for (int i = 0; i < length_uuid2; ++i) {
+      EEPROM.put(eep_aut + i, uuidString2[i]);
+    }
+    EEPROM.write(300, 60);
+    EEPROM.commit();
+  }
+  read_guid();
+  read_authkey();
+}
+
+String read_guid(void) {
+  String read_eeprom = "";
+  int i, ii = 0;
+  int eep_star = 301;
+  int end_guid = eep_star + SUPLA_GUID_SIZE;
+  String temp_read = "0x";
+  for (i = eep_star; i < end_guid + 16;  i = i + 1) {
+    temp_read += char(EEPROM.read(i));
+    read_eeprom += char(EEPROM.read(i));
+    if ( (i % 2) == 0) {
+      char *_guid = strcpy((char*)malloc(temp_read.length() + 1), temp_read.c_str());
+      GUID[ii] = strtoul( _guid, NULL, 16);
+      temp_read = "0x";
+      ii++;
+    }
+  }
+  return read_eeprom;
+}
+String read_authkey(void) {
+  String read_eeprom = "";
+  int i, ii = 0;
+  int eep_star = 321;
+  int end_authkey = eep_star + SUPLA_AUTHKEY_SIZE;
+  String temp_read = "0x";
+  for (i = eep_star; i < end_authkey + 16;  i = i + 1) {
+    temp_read += char(EEPROM.read(i));
+    read_eeprom += char(EEPROM.read(i));
+    if ( (i % 2) == 0) {
+      char *_authkey = strcpy((char*)malloc(temp_read.length() + 1), temp_read.c_str());
+      AUTHKEY[ii] = strtoul( _authkey, NULL, 16);
+      temp_read = "0x";
+      ii++;
+    }
+  }
+  return read_eeprom;
+}
+void setup() {
+  EEPROM.begin(1024);
+    guid_authkey();
+  
+}
+
